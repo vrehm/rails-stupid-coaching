445a68c35f5b29a104ecaadbd3d0e981b77ac818
Vincent REHM <vincentrehm@yahoo.fr>
2016-08-08 13:17:24 +0200

    root to ask

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'coach#ask'
   get 'answer', to: 'coach#answer'
-  get 'ask', to: 'coach#ask'
+  #get 'ask', to: 'coach#ask'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
